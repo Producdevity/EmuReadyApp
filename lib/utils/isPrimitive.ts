e04c39e0b2d1c137ea0d09b09467d90b512d73b5
Producdevity <y.gherbi.dev@gmail.com>
2025-06-15 19:06:13 +0200

    feat: add isJsonPrimitive function

---
+++ lib/utils/isPrimitive.ts
@@ -0,0 +1,11 @@
+import { isString, isBoolean, isNumber, isNullish } from 'remeda'
+
+/**
+ * Checks if the value is a JSON primitive.
+ * @param value - The value to check.
+ */
+export function isJsonPrimitive(value: unknown): boolean {
+  return (
+    isString(value) || isBoolean(value) || isNumber(value) || isNullish(value)
+  )
+}
